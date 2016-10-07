db9ad1c0e668cbe1b5eadd1a40b649a49f3e9107
Luminarys <postmaster@gensok.io>
2016-10-07 00:57:09 -0700

    Add slight improvements to Maybe class

--- acsl/maybe.hh
+++ acsl/maybe.hh
@@ -1,24 +1,34 @@
+/*
+ *  Maybe type implementation, inspired by Rust.
+ */
+
+#ifndef ACSL_MAYBE_HH
+#define ACSL_MAYBE_HH
+
 #include <functional>
 
 namespace acsl {
-template<typename T>
-class Maybe {
-    union {
-        T value_;
-        char none_state_;
-    };
-    bool has_value_ = false;
+    template<typename T>
+    class Maybe {
+        union {
+            T value_;
+            char none_state_;
+        };
+        bool has_value_ = false;
     public:
-        constexpr Maybe(): none_state_('\0') {}
-        Maybe(const T& v): value_(v), has_value_(true) {}
-        Maybe(T&& v): value_(std::move(v)), has_value_(true) {}
+        constexpr Maybe() : none_state_('\0') {}
+
+        Maybe(const T &v) : value_(v), has_value_(true) {}
+
         bool has_value() const {
             return this->has_value_;
         }
+
         bool is_none() const {
             return !this->has_value_;
         }
-        T unwrap_or(const T& v) {
+
+        T unwrap_or(const T &v) {
             if (this->has_value_) {
                 this->has_value_ = false;
                 return std::move(this->value_);
@@ -27,34 +37,43 @@ class Maybe {
             }
         }
 
-		template <typename U>
-        Maybe<U> map(std::function<U(T)> f) {
+        Maybe<const T &> as_ref() const {
             if (this->has_value_) {
-                this->has_value_ = false;
-                return new Maybe(f(std::move(this->value_)));
+                return Maybe<const T &>(this->value_);
             } else {
-                return new Maybe();
+                return Maybe<const T &>();
             }
         }
 
-    template <typename U>
-    U map_or(U v, std::function<U(T)> f) {
-        if (this->has_value_) {
-            this->has_value_ = false;
-            return f(std::move(this->value_));
-        } else {
-            return v;
+        template<typename U>
+        Maybe<U> map(std::function<U(T)> f) {
+            if (this->has_value_) {
+                this->has_value_ = false;
+                return Maybe<U>(f(std::move(this->value_)));
+            } else {
+                return Maybe<U>();
+            }
         }
-    }
 
-    template <typename U>
-    Maybe<U> and_then(std::function<Maybe<U>(T)> f) {
-        if (this->has_value_) {
-            this->has_value_ = false;
-            return f(std::move(this->value_));
-        } else {
-            return Maybe<U>();
+        template<typename U>
+        U map_or(U v, std::function<U(T)> f) {
+            if (this->has_value_) {
+                this->has_value_ = false;
+                return f(std::move(this->value_));
+            } else {
+                return v;
+            }
         }
-    }
-};
+
+        template<typename U>
+        Maybe<U> and_then(std::function<Maybe<U>(T)> f) {
+            if (this->has_value_) {
+                this->has_value_ = false;
+                return f(std::move(this->value_));
+            } else {
+                return Maybe<U>();
+            }
+        }
+    };
 }
+#endif
--- test/maybe.cc
+++ test/maybe.cc
@@ -3,20 +3,23 @@
 
 using namespace acsl;
 
-TEST_CASE( "Maybe", "[maybe]" ) {
-    auto mi = new Maybe<int>();
-    REQUIRE(mi->is_none());
-    REQUIRE(!mi->has_value());
-    REQUIRE(mi->unwrap_or(5) == 5);
+TEST_CASE("Maybe", "[maybe]") {
+    auto mi = Maybe<int>();
+    REQUIRE(mi.is_none());
+    REQUIRE(!mi.has_value());
+    REQUIRE(mi.unwrap_or(5) == 5);
 
-    mi = new Maybe<int>(5);
-    REQUIRE(mi->unwrap_or(0) == 5);
-    auto mn = mi->map<bool>([] (int i) { return true; });
+    mi = Maybe<int>(5);
+    REQUIRE(mi.as_ref().unwrap_or(10) == 5);
+    REQUIRE(mi.unwrap_or(0) == 5);
+
+    mi = Maybe<int>(5);
+    auto mn = mi.map<bool>([](int i) { return true; });
     REQUIRE(mn.unwrap_or(false));
 
-    mi = new Maybe<int>(5);
-    REQUIRE(mi->map_or<bool>(false, [] (int i) { return i == 5; }));
+    mi = Maybe<int>(5);
+    REQUIRE(mi.map_or<bool>(false, [](int i) { return i == 5; }));
 
-    mi = new Maybe<int>(5);
-    REQUIRE(mi->and_then<bool>([] (int i) { return Maybe<bool>(true); }).unwrap_or(false));
+    mi = Maybe<int>(5);
+    REQUIRE(mi.and_then<bool>([](int i) { return Maybe<bool>(true); }).unwrap_or(false));
 }
